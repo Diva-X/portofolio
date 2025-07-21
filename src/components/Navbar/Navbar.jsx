162db6a0e42a9a1e6bc2b2dd8530f6e738ec3708
Sophie Rimbaud <rimbaudsophie@outlook.fr>
2025-07-21 15:46:17 +0200

    finir navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,18 +5,18 @@ function Navbar() {
   return (
     <header>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container">
+        <div className="container-fluid">
           <NavLink className="navbar-brand" to="/">John Doe</NavLink>
 
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
             <span className="navbar-toggler-icon"></span>
           </button>
 
