59767f42f18877243f5bed44e3550d402baafedf
Sophie Rimbaud <rimbaudsophie@outlook.fr>
2025-07-21 18:41:38 +0200

    Mise en page du texte et des compétences avec les barres, mais pas l'image

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
-// src/pages/Home/Home.jsx
 import React from 'react';
 import Hero from '../../components/Hero/Hero';
+import About from '../../components/About/About';
 
 function Home() {
   return (
     <>
       <Hero />
+      <About />
     </>
   );
 }
